adda904760f7ba668921b0e64d0b1175b2ecdd6d
shariar007 <uiu.151007@gmail.com>
2021-10-08 23:11:49 +0600

    apply click event in image

---
+++ confusion/src/components/MenuComponent.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {Card, CardImg, CardImgOverlay, CardBody, CardHeader, CardTitle, CardText} from "reactstrap";
+
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    render() {
+        // var imgStyle = {
+        //     minWidth: "40px",
+        //     // width: "200px",
+        // };
+        // style={imgStyle}
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <Card key={dish.id} onClick={()=> this.onDishSelect(dish)}>
+                        <CardImg src={dish.image} alt={dish.name} width="100%"/>
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                     {menu}
+                </div>
+                <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
